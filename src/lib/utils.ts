0396bb091cc4b5191d2b549e3e9c2265d7e7a687
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-04-28 07:50:05 +0700

    add helper func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const generatePagination = (totalPages: number, currentPage: number) => {
+  const visiblePages: (number | string)[] = [];
+  const range = 1;
+
+  for (let i = 0; i < totalPages; i++) {
+    if (i >= currentPage - range && i <= currentPage + range) {
+      visiblePages.push(i);
+    } else if (visiblePages[visiblePages.length - 1] !== "...") {
+      visiblePages.push("...");
+    }
+  }
+  return visiblePages;
+};
