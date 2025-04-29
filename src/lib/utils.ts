26e9edde14c8636eea1f363f1c7de6ee081bea72
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-04-29 14:14:50 +0700

    add helper func for displaying currency

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -18,3 +18,19 @@ export const generatePagination = (totalPages: number, currentPage: number) => {
   }
   return visiblePages;
 };
+
+export const formatRupiah = (value: string): string => {
+  const numberString = value.replace(/[^,\d]/g, "");
+  const split = numberString.split(",");
+  const sisa = split[0].length % 3;
+  let rupiah = split[0].substr(0, sisa);
+  const ribuan = split[0].substr(sisa).match(/\d{3}/g);
+
+  if (ribuan) {
+    const separator = sisa ? "." : "";
+    rupiah += separator + ribuan.join(".");
+  }
+
+  rupiah = split[1] !== undefined ? rupiah + "," + split[1] : rupiah;
+  return rupiah;
+};
