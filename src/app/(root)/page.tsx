42689b69ec878cdcad7b6c3aed1d0b1b9ebe88fb
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-05-02 08:49:08 +0700

    render data count

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -7,6 +7,7 @@ import {
   createColumnHelper,
   flexRender,
   getCoreRowModel,
+  getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 import {
@@ -21,7 +22,6 @@ import { format } from "date-fns";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
@@ -48,6 +48,7 @@ import DailyPatientMealForm from "@/components/DailyPatientMealForm";
 import { id } from "date-fns/locale";
 import toast from "react-hot-toast";
 import { DateTimePicker } from "@/components/ui/datetime-picker";
+import { pivotMealMatrix } from "@/lib/utils";
 
 const HomePage = () => {
   const isMobile = useIsMobile();
@@ -56,11 +57,14 @@ const HomePage = () => {
   const [roomTypes, setRoomTypes] = useState<RoomType[]>([]);
   const [rooms, setRooms] = useState<Room[]>([]);
   const [mealTypes, setMealTypes] = useState<MealType[]>([]);
+  const [diets, setDiets] = useState<Diet[]>([]);
   const [data, setData] = useState<DailyPatientMeal[]>([]);
   const [dialogOpen, setDialogOpen] = useState(false);
   const [selectedDailyMeal, setSelectedDailyMeal] = useState<
     DailyPatientMeal | undefined
   >();
+  const [matrixMealCount, setMatrixMealCount] = useState<MatrixRow[]>([]);
+  const [matrixMealCountAll, setMatrixMealCountAll] = useState<MatrixRow[]>([]);
 
   useEffect(() => {
     const fetchRequiredData = async () => {
@@ -74,6 +78,9 @@ const HomePage = () => {
           `${process.env.NEXT_PUBLIC_BACKEND_URL}/meal-type`,
         );
         setMealTypes(res.data.data as MealType[]);
+
+        res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/diet`);
+        setDiets(res.data.data as Diet[]);
       } catch (err) {
         console.error(err);
         toast.error(String(err));
@@ -98,8 +105,28 @@ const HomePage = () => {
       const formattedDate = format(date, "yyyy-MM-dd");
       const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/daily-patient-meal/filter?date=${formattedDate}&roomType=${roomType}`;
       const res = await axios.get(url);
-      console.log(url);
-      setData(res.data.data);
+      setData(res.data.data as DailyPatientMeal[]);
+    } catch (err) {
+      console.error(err);
+      toast.error(String(err));
+    }
+  };
+
+  const fetchCountData = async () => {
+    try {
+      if (!date || mealTypes.length === 0) return;
+      const formattedDate = format(date, "yyyy-MM-dd");
+      let url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/daily-patient-meal/count?date=${formattedDate}&roomType=${roomType}`;
+      let res = await axios.get(url);
+      setMatrixMealCount(
+        pivotMealMatrix(res.data.data as MealMatrixEntry[], mealTypes),
+      );
+
+      url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/daily-patient-meal/count?date=${formattedDate}&roomType=`;
+      res = await axios.get(url);
+      setMatrixMealCountAll(
+        pivotMealMatrix(res.data.data as MealMatrixEntry[], mealTypes),
+      );
     } catch (err) {
       console.error(err);
       toast.error(String(err));
@@ -109,7 +136,8 @@ const HomePage = () => {
   useEffect(() => {
     if (!date || !roomType) return;
     fetchFilteredData();
-  }, [date, roomType]);
+    fetchCountData();
+  }, [date, roomType, mealTypes]);
 
   const columnHelper = createColumnHelper<DailyPatientMeal>();
   const columns: ColumnDef<DailyPatientMeal>[] = [
@@ -117,11 +145,13 @@ const HomePage = () => {
       id: "medicalRecordNumber",
       header: "Nomor MR",
       cell: (info) => info.getValue(),
+      size: 90,
     }),
     columnHelper.accessor("patient.name", {
       id: "patientName",
       header: "Nama Pasien",
       cell: (info) => info.getValue(),
+      size: 150,
     }),
     columnHelper.accessor("patient.dateOfBirth", {
       id: "patientDateOfBirth",
@@ -130,26 +160,51 @@ const HomePage = () => {
         const rawDate = info.getValue();
         return rawDate ? format(new Date(rawDate), "PPP", { locale: id }) : "-";
       },
+      size: 120,
     }),
     columnHelper.accessor("room.roomNumber", {
       id: "roomNumber",
-      header: "Nomor Kamar",
+      header: "No. Kamar",
       cell: (info) => info.getValue(),
+      size: 70,
     }),
     columnHelper.accessor("room.treatmentClass", {
       id: "treatmentClass",
       header: "Kelas Perawatan",
       cell: (info) => info.getValue(),
+      size: 100,
     }),
-    columnHelper.accessor("mealType.name", {
-      id: "mealTypeName",
+    columnHelper.accessor("mealType.code", {
+      id: "mealTypeCode",
       header: "Jenis Makanan",
       cell: (info) => info.getValue(),
+      size: 100,
+    }),
+    columnHelper.accessor("diets", {
+      id: "diets",
+      header: "Diet",
+      cell: (info) =>
+        info
+          .getValue()
+          .map((diet) => diet.code)
+          .join(", "),
+      size: 100,
+    }),
+    columnHelper.accessor("patient.allergies", {
+      id: "patientAllergies",
+      header: "Alergi",
+      cell: (info) =>
+        info
+          .getValue()
+          .map((allergy) => allergy.code)
+          .join(", "),
+      size: 100,
     }),
     columnHelper.accessor("updatedAt", {
       id: "updatedAt",
       header: "Updated At",
       cell: (info) => info.getValue(),
+      sortingFn: "datetime",
     }),
   ] as ColumnDef<DailyPatientMeal>[];
 
@@ -157,10 +212,17 @@ const HomePage = () => {
     columns,
     data,
     getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
     initialState: {
       columnVisibility: {
         updatedAt: false,
       },
+      sorting: [
+        {
+          id: "updatedAt",
+          desc: true,
+        },
+      ],
     },
   });
 
@@ -207,12 +269,18 @@ const HomePage = () => {
 
       {roomType && data.length > 0 && (
         <Table>
-          <TableCaption>Tes</TableCaption>
-
           <TableHeader>
             <TableRow>
               {table.getFlatHeaders().map((header) => (
-                <TableHead key={header.id}>
+                <TableHead
+                  key={header.id}
+                  className="truncate whitespace-normal"
+                  style={{
+                    width: `${header.getSize()}px`,
+                    minWidth: `${header.getSize()}px`,
+                    maxWidth: `${header.getSize()}px`,
+                  }}
+                >
                   {flexRender(
                     header.column.columnDef.header,
                     header.getContext(),
@@ -257,19 +325,17 @@ const HomePage = () => {
             <DailyPatientMealForm
               rooms={rooms}
               mealTypes={mealTypes}
+              diets={diets}
               initialData={selectedDailyMeal}
               onSuccess={() => {
                 setDialogOpen(false);
                 setSelectedDailyMeal(undefined);
                 fetchFilteredData();
+                fetchCountData();
               }}
               className="px-4"
             />
-            <DrawerFooter className="pt-2">
-              {/* <DrawerClose asChild>
-                <Button variant="outline">Cancel</Button>
-              </DrawerClose> */}
-            </DrawerFooter>
+            <DrawerFooter className="pt-2"></DrawerFooter>
           </DrawerContent>
         </Drawer>
       ) : (
@@ -286,15 +352,91 @@ const HomePage = () => {
             <DailyPatientMealForm
               rooms={rooms}
               mealTypes={mealTypes}
+              diets={diets}
               initialData={selectedDailyMeal}
               onSuccess={() => {
                 setDialogOpen(false);
                 setSelectedDailyMeal(undefined);
                 fetchFilteredData();
+                fetchCountData();
               }}
             />
           </DialogContent>
         </Dialog>
+      )}
+
+      {date && roomType && matrixMealCount.length > 0 && (
+        <div className="mt-8 flex flex-col">
+          <p>
+            Rekapitulasi Permintaan Makanan{" "}
+            <span className="bg-secondary rounded-sm p-1 font-bold">
+              {roomTypes.find((rt) => rt.id === roomType)?.name}
+            </span>{" "}
+            - {format(date, "PPP", { locale: id })}
+          </p>
+
+          <Table className="w-1/3">
+            <TableHeader>
+              <TableRow>
+                <TableHead className="truncate whitespace-normal">
+                  Kelas Perawatan
+                </TableHead>
+                {mealTypes.map((mt) => (
+                  <TableHead key={mt.id}>{mt.code}</TableHead>
+                ))}
+              </TableRow>
+            </TableHeader>
+
+            <TableBody>
+              {matrixMealCount.map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell>{row.treatmentClass}</TableCell>
+                  {mealTypes.map((mt) => (
+                    <TableCell key={`${row.treatmentClass}-${mt.code}`}>
+                      {row[mt.code] || 0}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      )}
+
+      {date && roomType && matrixMealCountAll.length > 0 && (
+        <div className="flex flex-col">
+          <p>
+            Rekapitulasi Permintaan Makanan{" "}
+            <span className="bg-secondary rounded-sm p-1 font-bold">
+              Semua Ruangan
+            </span>{" "}
+            - {format(date, "PPP", { locale: id })}
+          </p>
+
+          <Table className="w-1/3">
+            <TableHeader>
+              <TableRow>
+                <TableHead>Kelas Perawatan</TableHead>
+                {mealTypes.map((mt) => (
+                  <TableHead key={mt.id}>{mt.code}</TableHead>
+                ))}
+              </TableRow>
+            </TableHeader>
+
+            <TableBody>
+              {matrixMealCountAll.map((row, index) => (
+                <TableRow key={index}>
+                  <TableCell>{row.treatmentClass}</TableCell>
+                  {mealTypes.map((mt) => (
+                    <TableCell key={`${row.treatmentClass}-${mt.code}`}>
+                      {row[mt.code] || 0}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
       )}
     </div>
   );
