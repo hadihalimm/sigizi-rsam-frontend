4e7442eedddf5a7c7c6c16edd685ea3a620159b2
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-05-05 08:47:57 +0700

    add user-scalable meta property

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0"
+          content="width=device-width, initial-scale=1.0 user-scalable=yes"
         ></meta>
       </head>
       <body className={`${inter.variable} font-inter antialiased`}>
