76530c581631561252073efc06efb9f1a57efb06
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-05-15 15:52:13 +0700

    remove isAuthenticated property

--- src/hooks/use-session.ts
+++ src/hooks/use-session.ts
@@ -2,14 +2,12 @@ import { create } from "zustand";
 
 interface SessionStore {
   user: UserSession | null;
-  isAuthenticated: boolean;
   setSession: (user: UserSession) => void;
   clearSession: () => void;
 }
 
 const useSessionStore = create<SessionStore>((set) => ({
   user: null,
-  isAuthenticated: false,
   setSession: (user) => set({ user }),
   clearSession: () => set({ user: null }),
 }));
