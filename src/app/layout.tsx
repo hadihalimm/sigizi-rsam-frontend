5e0f058b2d6dbfc78eb0deeb5f123e9cdbcdb8df
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-05-17 20:34:51 +0700

    merriweather font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Merriweather } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
@@ -7,6 +7,12 @@ import { ThemeProvider } from "@/components/ui/theme-provider";
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
+});
+
+const merriweather = Merriweather({
+  variable: "--font-merriweather",
+  subsets: ["latin"],
+  weight: ["400", "700", "900"],
 });
 
 export const metadata: Metadata = {
@@ -27,10 +33,12 @@ export default function RootLayout({
           content="width=device-width, initial-scale=1.0 user-scalable=yes"
         ></meta>
       </head>
-      <body className={`${inter.variable} font-inter antialiased`}>
+      <body
+        className={`${inter.variable} ${merriweather.variable} font-inter antialiased`}
+      >
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
