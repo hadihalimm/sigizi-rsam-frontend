79b222877a39f08d58b2459f04ca328ac8345e85
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-06-23 08:44:34 +0700

    add reusable combobox based on shadcn

---
+++ src/components/ui/combobox.tsx
@@ -0,0 +1,91 @@
+"use client";
+import { CheckIcon, ChevronDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useState } from "react";
+
+export interface Option {
+  value: string;
+  label: string;
+}
+
+interface ComboboxProps {
+  options: Option[];
+  value: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+  className?: string;
+}
+
+export function Combobox({
+  options,
+  value,
+  onChange,
+  placeholder,
+  className,
+}: ComboboxProps) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className={className}>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className={cn("w-[200px] justify-between", className)}
+          >
+            {options.find((opt) => opt.value === value)?.label ?? placeholder}
+            {/* {value
+              ? options.find((opt) => opt.value === value)?.label
+              : placeholder} */}
+            <ChevronDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className={cn("w-[200px] p-0", className)}>
+          <Command>
+            <CommandInput placeholder="Search..." />
+            <CommandList>
+              <CommandEmpty>No option found.</CommandEmpty>
+              <CommandGroup>
+                {options.map((opt) => (
+                  <CommandItem
+                    key={opt.value}
+                    value={opt.label}
+                    onSelect={() => {
+                      onChange(opt.value);
+                      setOpen(false);
+                    }}
+                  >
+                    <CheckIcon
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        value === opt.value ? "opacity-100" : "opacity-0",
+                      )}
+                    />
+                    {opt.label}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
